b5f216bc0b771632b3baf1838534831c0ecf06a8
Chris Liloba <chrislilobalinyulu@gmail.com>
2024-10-03 13:53:47 +0300

    Add files via upload

---
+++ ChatRoom Connection/server.py
@@ -0,0 +1,57 @@
+' CHAT ROOM CONNECTION - Client-To-Client '
+
+import threading
+import socket
+from datetime import datetime
+
+host = '127.0.0.1'
+port = 59000
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+clients = []
+aliases = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            alias = aliases[index]
+            broadcast(f'{alias} has left the chat room!! '.encode('utf-8'))
+            aliases.remove(alias)
+            break
+
+# Main Function to receive the client;s connection
+def receive():
+    while True:
+        print('Server is running and listening......')
+        client, address = server.accept()
+        print(f'  CONNECTION IS ESTABLISHED WITH {str(address)}')
+        client.send(' alias??'.encode('utf-8'))
+        alias = client.recv(1024)
+        aliases.append(alias)
+        clients.append(client)
+        print(f' The alias of this client is {alias}'.encode('utf-8'))
+        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+        client.send(' You are now connected! '.encode('utf-8'))
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+
+if __name__ == "__main__":
+    receive()
+
+
+
+
